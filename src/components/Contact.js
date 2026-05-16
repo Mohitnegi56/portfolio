7e7e75d1a2396b82356ba37121c6aa68141eb60e
Mohitnegi56 <shashinegi597@gmail.com>
2026-05-16 08:20:25 +0530

    Update API URL for production

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ export const Contact = () => {
     setButtonText("Sending...");
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://backend-portfolio-bcwt.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
